333ef88eae6fec22d79a546be14fe4bff8942625
Victor Mirzac <Victor.Mirzac@endava.com>
2018-08-30 00:28:29 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double myPounds = 5;
+        double myKilograms = myPounds*0.45359237;
+
+        System.out.println("You have " + myKilograms + " kilograms");
+
+    }
+}
